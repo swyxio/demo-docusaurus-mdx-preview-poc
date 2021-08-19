8f6e092630b1680e9f3d93b92cb04e90d5c679c6
sw-yx <shawnthe1@gmail.com>
2021-08-18 17:33:29 -0700

    docs

--- src/pages/index.js
+++ src/pages/index.js
@@ -7,24 +7,6 @@ import styles from './index.module.css';
 import HomepageFeatures from '../components/HomepageFeatures';
 import Renderer from './Renderer'
 
-function HomepageHeader() {
-  const {siteConfig} = useDocusaurusContext();
-  return (
-    <header className={clsx('hero hero--primary', styles.heroBanner)}>
-      <div className="container">
-        <h1 className="hero__title">{siteConfig.title}</h1>
-        <p className="hero__subtitle">{siteConfig.tagline}</p>
-        <div className={styles.buttons}>
-          <Link
-            className="button button--secondary button--lg"
-            to="/docs/intro">
-            Docusaurus Tutorial - 5min ⏱️
-          </Link>
-        </div>
-      </div>
-    </header>
-  );
-}
 
 export default function Home() {
   const {siteConfig} = useDocusaurusContext();
@@ -32,9 +14,9 @@ export default function Home() {
     <Layout
       title={`Hello from ${siteConfig.title}`}
       description="Description will go into a meta tag in <head />">
-      <HomepageHeader />
-      <main>
-        <HomepageFeatures />
+      <h1> Welcome!</h1>
+      <main style={{ margin: 20 }}>
+        Head to the <Link to="/docs/intro">Intro for the demo</Link>
       </main>
     </Layout>
   );
